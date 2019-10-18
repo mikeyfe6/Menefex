46dd2d4ba338ccee26e4f63dcf8e43b4cf47263c
Michael Fransman <mikeyfe6@users.noreply.github.com>
2019-10-18 03:38:28 +0200

    Working Copy: testing the mobile approach of coding!

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => {
     <Layout>
       <SEO title="Welkom" />
       <h1>Hello.</h1>
-      <h2>I&apos;m Michael from Gimmix</h2>
+      <h2>Ik ben Michael en dit is mijn bedrijf: Gimmix</h2>
       <p>
         Heb je een fantastische webdeveloper nodig?{" "}
         <Link to="/contact">Klik hier</Link>
