0c9f58fc84c943701373d69f07cc46e312f70be0
Michael Fransman <mikeyfe6@gimmix.nl>
2019-10-18 17:22:11 +0200

    Update form.js
    And again.....

--- src/components/form.js
+++ src/components/form.js
@@ -15,6 +15,7 @@ const Form = () => {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
+        <input type="hidden" name="form-name" value="contact" />
         <p>
           <label>
             Your Name: <input type="text" name="name" />
