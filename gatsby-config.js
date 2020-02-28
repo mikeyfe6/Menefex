f56182d1038690227d3482d1b91f165b04e0c869
Michael Fransman <mikeyfe6@gimmix.nl>
2020-02-28 11:58:19 +0100

    Inserted Gtag Manager.

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,6 +24,26 @@ module.exports = {
         head: true,
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-tagmanager',
+      options: {
+        id: 'GTM-5X6VS2L',
+
+        // Include GTM in development.
+        // Defaults to false meaning GTM will only be loaded in production.
+        includeInDevelopment: false,
+
+        // datalayer to be set before GTM is loaded
+        // should be an object or a function that is executed in the browser
+        // Defaults to null
+        defaultDataLayer: { platform: 'gatsby' },
+
+        // Specify optional GTM environment details.
+        // gtmAuth: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_AUTH_STRING",
+        // gtmPreview: "YOUR_GOOGLE_TAGMANAGER_ENVIRONMENT_PREVIEW_NAME",
+        // dataLayerName: "YOUR_DATA_LAYER_NAME",
+      },
+    },
     `gatsby-plugin-catch-links`,
     {
       resolve: 'gatsby-plugin-eslint',
