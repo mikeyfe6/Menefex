e1e6777c5229fa71af32421bf451d26331a51ab9
Michael Fransman <mikeyfe6@gimmix.nl>
2020-03-09 00:03:05 +0100

    Testing og:url

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,7 @@ module.exports = {
   siteMetadata: {
     siteUrl: `https://gimmix.nl`,
     url: 'https://gimmix.nl',
-    image: '/static/Gimmix-logo.png',
+    image: '/src/img/Gimmix-logo.png',
     title: 'Gimmix',
     titleTemplate: '%s · Gimmix',
     author: 'Michael Fransman',
