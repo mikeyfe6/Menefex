921e75b70ee8ea07d47dbe2e68a095d69abd982c
Michael Fransman <mikeyfe6@gimmix.nl>
2020-03-11 21:09:01 +0100

    Updated style for contactbox!

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -111,22 +111,68 @@ const Form = () => {
             >
               Versturen
             </button>
-            <br />
+            <div className="clr" />
+
             <div>
-              <div className={formStyles.contactbox}>
-                &nbsp;
-                <FontAwesomeIcon icon="mobile" size="lg" color="#656565" />
-                <p className={formStyles.contacttel}>
-                  <a href="tel:0611054318">: +31 6 11 05 43 18</a>
-                </p>
-                <br />
-                <FontAwesomeIcon icon="paper-plane" size="lg" color="#656565" />
-                <p className={formStyles.contactmail}>
-                  <a href="mailto:contact@gimmix.nl">: contact@gimmix.nl</a>
-                </p>
+              <div className={`${formStyles.contactbox}`}>
+                <div className="row">
+                  <div className="col-md">
+                    <p>
+                      <u>Wij zijn bereikbaar van:</u> <br /> <br />
+                      <span className={formStyles.opentijd}>
+                        ma: &nbsp; 9:00 - 19:00
+                        <br />
+                        di: &nbsp; 9:00 - 19:00
+                        <br />
+                        wo: &nbsp; 9:00 - 19:00
+                        <br />
+                        do: &nbsp; 9:00 - 19:00
+                        <br />
+                        vri: &nbsp; 9:00 - 19:00
+                        <br />
+                        <br />
+                        <b>za - zo: &nbsp; gesloten</b>
+                        <br />
+                        <br />
+                      </span>
+                    </p>
+                  </div>
+                  <div className="col-md">
+                    &nbsp;
+                    <FontAwesomeIcon icon="mobile" size="lg" color="#656565" />
+                    <p className={formStyles.contacttel}>
+                      <a href="tel:0611054318">: +31 6 11 05 43 18</a>
+                    </p>
+                    <br />
+                    <FontAwesomeIcon
+                      icon="paper-plane"
+                      size="lg"
+                      color="#656565"
+                    />
+                    <p className={formStyles.contactmail}>
+                      <a href="mailto:contact@gimmix.nl">: contact@gimmix.nl</a>
+                    </p>
+                    <br />
+                    <br />
+                    <p className={formStyles.contactgev}>
+                      <b>Gimmix Webmediabedrijf</b> <br />
+                      <span className={formStyles.gimmixgev}>
+                        <u>Kamer van koophandel-nummer:</u> <br />{' '}
+                        <i>76045315</i>
+                        <br />
+                        <u>BTW-nummer: </u>
+                        <br />
+                        <i>NL 003040579B17</i>
+                        <br />
+                        <u>BANK-nummer: </u>
+                        <br />
+                        <i>NL10 BUNQ 2038 8619 94</i>
+                      </span>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
-            <div className="clr" />
           </form>
         </div>
       </div>
