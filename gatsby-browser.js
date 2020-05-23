ca6bbb68c552465f6598135ae98056517d10ec0c
Michael Fransman <mikeyfe6@gimmix.nl>
2020-05-23 15:21:03 +0200

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,1 @@
-/* eslint-disable no-alert */
-/* eslint-disable import/prefer-default-export */
-
-// exports.onServiceWorkerUpdateFound = () => window.location.reload(true);
-export const onServiceWorkerUpdateReady = () => {
-  const answer = window.confirm(
-    `This application has been updated. ` +
-      `Reload to display the latest version?`,
-  );
-  if (answer === true) {
-    window.location.reload();
-  }
-};
+exports.onServiceWorkerUpdateReady = () => window.location.reload(true);
