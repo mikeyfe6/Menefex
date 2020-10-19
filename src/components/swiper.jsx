011477aba6db72e62621b945b4a32723ef2f4d5c
Michael Fransman <mikeyfe6@gimmix.nl>
2020-10-19 08:44:41 +0200

    cleaned the codebase

--- src/components/swiper.jsx
+++ src/components/swiper.jsx
@@ -5,7 +5,6 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 import '../styles/swipering.scss';
 import 'swiper/swiper.scss';
-import 'swiper/components/navigation/navigation.scss';
 import 'swiper/components/pagination/pagination.scss';
 import 'swiper/components/scrollbar/scrollbar.scss';
 import 'swiper/components/autoplay';
--- src/posts/webdevelopment.md
+++
@@ -1,6 +0,0 @@
----
-title: 'Dit gaat mijn leven veranderen in een mum van tijd'
-date: '2019-06-02'
----
-
-Ik leer, ik leef, ik geniet!
