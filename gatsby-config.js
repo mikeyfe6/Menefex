6bb745b944c95f25c651d8170272d88d19f98f0e
Michael Fransman <mikeyfe6@gimmix.nl>
2020-10-30 14:46:10 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,18 @@ module.exports = {
     authorEmail: 'michaelfransman@gimmix.nl',
   },
   plugins: [
-    `gatsby-plugin-netlify`,
+    {
+      resolve: `gatsby-plugin-netlify`,
+      options: {
+        // headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
+        // allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
+        mergeSecurityHeaders: false, // boolean to turn off the default security headers
+        mergeLinkHeaders: false, // boolean to turn off the default gatsby js headers
+        // mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        // transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
+        // generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
+      },
+    },
     {
       resolve: 'gatsby-plugin-google-tagmanager',
       options: {
