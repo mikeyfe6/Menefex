a692f233a48487753a8c950f1c6f534af5d1a8d1
Michael Fransman <mikeyfe6@gimmix.nl>
2020-10-30 14:52:01 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,9 +26,9 @@ module.exports = {
       options: {
         // headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
         // allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: false, // boolean to turn off the default security headers
-        mergeLinkHeaders: false, // boolean to turn off the default gatsby js headers
-        // mergeCachingHeaders: true, // boolean to turn off the default caching headers
+        mergeSecurityHeaders: true, // boolean to turn off the default security headers
+        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
+        // true, // boolean to turn off the default caching headers
         // transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
         // generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
       },
