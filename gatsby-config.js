af8382b26d896df431a268b2007e5c19cfeb14d6
Michael Fransman <mikeyfe6@gimmix.nl>
2020-11-03 17:42:00 +0100

    updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -194,7 +194,7 @@ module.exports = {
       resolve: `gatsby-plugin-sharp`,
       options: {
         base64Width: 20,
-        forceBase64Format: ``, // valid formats: png,jpg,webp
+        forceBase64Format: `webp`, // valid formats: png,jpg,webp
         useMozJpeg: process.env.GATSBY_JPEG_ENCODER === `MOZJPEG`,
         stripMetadata: true,
         defaultQuality: 50,
