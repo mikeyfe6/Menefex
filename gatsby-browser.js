ef4656fcfda1baf7a3636d84966b5e533df86bdb
Michael Fransman <mikeyfe6@gimmix.nl>
2021-01-07 06:40:06 +0100

    updated packages

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,14 @@
-// export function onClientEntry() {
-//   document.querySelector('body').style.display = 'none';
-//   console.log('GXWMB: Gimmix is aan het laden...');
-// }
-
-export function onInitialClientRender() {
-  document.querySelector('body').style.display = 'block';
-
-  console.log('GXWMB: Welkom bij Gimmix Webmediabedrijf !');
-}
-
 export function onServiceWorkerUpdateReady() {
   window.location.reload();
   console.log('GXWMB: Gimmix is geupdated !');
+}
+
+export function onClientEntry() {
+  document.querySelector('body').style.display = 'none';
+  console.log('GXWMB: Aan het laden ...');
+}
+
+export function onInitialClientRender() {
+  document.querySelector('body').style.display = 'block';
+  console.log('GXWMB: Welkom bij Gimmix Webmediabedrijf !');
 }
