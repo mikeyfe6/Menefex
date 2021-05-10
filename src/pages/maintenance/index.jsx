805b0eb9725d4726b08f202bf6873c6f9d4bc6e0
Michael Fransman <mikeyfe6@gimmix.nl>
2021-05-10 06:35:30 +0200

    updated packaged & edited maintenance page

--- src/pages/maintenance/index.jsx
+++ src/pages/maintenance/index.jsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 export const Maintenance = () => (
   <div>
-    We zijn snel weer online! En wel onder een nieuwe naam: Menefex. Zelfde
-    kwaliteit, zelfde doelen.
+    <h1
+      style={{
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+      }}
+    >
+      We zijn snel weer online! En wel onder een nieuwe naam: Menefex. Zelfde
+      kwaliteit, zelfde doelen.
+    </h1>
   </div>
 );
 
