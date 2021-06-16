594dbfb6323f893df3e069db1dbafe54484fd54a
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-06-16 03:39:48 +0200

    Update sendmail.jsx

--- functions/sendmail/sendmail.jsx
+++ functions/sendmail/sendmail.jsx
@@ -3,8 +3,8 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
-  to: 'test@example.com', // Change to your recipient
-  from: 'test@example.com', // Change to your verified sender
+  to: 'info@menefex.nl', // Change to your recipient
+  from: 'info@menefex.nl', // Change to your verified sender
   subject: 'Sending with SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
