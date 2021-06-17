95f89e5ef13cb330ae5a04f9d7f5bc6ad8abe41a
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-06-17 05:28:22 +0200

    Update sendmail.js

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -1,43 +1,62 @@
-const sendgrid = require('@sendgrid/mail');
+// const sendgrid = require('@sendgrid/mail');
 
-sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
-const message = {
-  from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+// sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
+// const message = {
+//   from: process.env.SENDGRID_AUTHORIZED_EMAIL,
+// };
+// console.log(message);
+
+// exports.handler = (req, res) => {
+//   try {
+//     if (req.method !== 'POST') {
+//       res.json({ message: 'Try a POST!' });
+//     }
+
+//     if (req.body) {
+//       message.to = req.body.email;
+//       message.subject = req.body.subject;
+//       message.text = req.body.text;
+//       message.html = req.body.text;
+//     }
+//     console.log(req.body);
+
+//     return sendgrid.send(message).then(
+//       () => {
+//         res.status(200).json({
+//           message: 'I will send email',
+//         });
+//       },
+//       // eslint-disable-next-line consistent-return
+//       (error) => {
+//         console.error(error);
+//         if (error.response) {
+//           return res.status(500).json({
+//             error: error.response,
+//           });
+//         }
+//       },
+//     );
+//   } catch (err) {
+//     console.log(err);
+//     return res.status(500).json({ message: 'There was an error', error: err });
+//   }
+// };
+
+const sgMail = require('@sendgrid/mail');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'info@menefex.nl', // Change to your recipient
+  from: 'info@menefex.nl', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
-console.log(message);
-
-exports.handler = (req, res) => {
-  try {
-    if (req.method !== 'POST') {
-      res.json({ message: 'Try a POST!' });
-    }
-
-    if (req.body) {
-      message.to = req.body.email;
-      message.subject = req.body.subject;
-      message.text = req.body.text;
-      message.html = req.body.text;
-    }
-    console.log(req.body);
-
-    return sendgrid.send(message).then(
-      () => {
-        res.status(200).json({
-          message: 'I will send email',
-        });
-      },
-      // eslint-disable-next-line consistent-return
-      (error) => {
-        console.error(error);
-        if (error.response) {
-          return res.status(500).json({
-            error: error.response,
-          });
-        }
-      },
-    );
-  } catch (err) {
-    console.log(err);
-    return res.status(500).json({ message: 'There was an error', error: err });
-  }
-};
+sgMail
+  .send(msg)
+  .then(() => {
+    console.log('Email sent');
+  })
+  .catch((error) => {
+    console.error(error);
+  });
