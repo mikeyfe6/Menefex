abd62808a6b265799bb191f21e76def29e5307bf
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-06-17 05:39:19 +0200

    Update sendmail.js

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -45,18 +45,21 @@
 const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-const msg = {
-  to: 'info@menefex.nl', // Change to your recipient
-  from: 'info@menefex.nl', // Change to your verified sender
-  subject: 'Sending with SendGrid is Fun',
-  text: 'and easy to do anywhere, even with Node.js',
-  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+
+exports.handler = () => {
+  const msg = {
+    to: 'info@menefex.nl', // Change to your recipient
+    from: 'info@menefex.nl', // Change to your verified sender
+    subject: 'Sending with SendGrid is Fun',
+    text: 'and easy to do anywhere, even with Node.js',
+    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log('Email sent');
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 };
-sgMail
-  .send(msg)
-  .then(() => {
-    console.log('Email sent');
-  })
-  .catch((error) => {
-    console.error(error);
-  });
