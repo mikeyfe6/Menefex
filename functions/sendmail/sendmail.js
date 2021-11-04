ce9d9a437afba604b27744cb7f3e53eaf4730cff
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-11-04 04:26:30 +0100

    Update sendmail.js

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -3,18 +3,18 @@ const sgMail = require('@sendgrid/mail');
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-exports.handler = async function (req, res) {
-  const data = JSON.parse(req.body);
-  const { name, subject, text, email, company, tel, tijdstip } = data;
+// exports.handler = async function (req, res) {
+const data = JSON.parse(req.body);
+const { name, subject, text, email, company, tel, tijdstip } = data;
 
-  const msg = {
-    to: 'contact@menefex.nl', // Change to your recipient
-    from: process.env.SENDGRID_AUTHORIZED_EMAIL, // Change to your verified sender
-    subject: `Mail ontvangen betreft: '${subject || tijdstip}'`,
-    text,
-    html: `<p><strong>Lead ontvangen</strong> <small>van</small> <strong>${name}</strong> - ${
-      email || '*geen email'
-    } - ${company || '*geen bedrijf'}</p>
+const msg = {
+  to: 'contact@menefex.nl', // Change to your recipient
+  from: process.env.SENDGRID_AUTHORIZED_EMAIL, // Change to your verified sender
+  subject: `Mail ontvangen betreft: '${subject || tijdstip}'`,
+  text,
+  html: `<p><strong>Lead ontvangen</strong> <small>van</small> <strong>${name}</strong> - ${
+    email || '*geen email'
+  } - ${company || '*geen bedrijf'}</p>
 
       <p><u><strong><small>Bericht</small></strong></u><br />${text}</p>
    
@@ -24,12 +24,17 @@ exports.handler = async function (req, res) {
       </strong></i></p>
       
       <p><small>Telnr:</small> <strong>${tel}</strong></p>`,
-  };
+};
 
+(async () => {
   try {
     await sgMail.send(msg);
   } catch (error) {
     console.error(error);
-    console.log(res);
+
+    if (error.res) {
+      console.error(error.res.body);
+    }
   }
-};
+})();
+// };
