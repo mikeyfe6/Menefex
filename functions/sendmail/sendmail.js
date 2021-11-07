92cb2bfecf47baf7ec64e5d3b063accb1df3bb44
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2021-11-07 17:55:45 +0100

    Update sendmail.js

--- functions/sendmail/sendmail.js
+++ functions/sendmail/sendmail.js
@@ -8,22 +8,36 @@ exports.handler = async (event) => {
   const { name, subject, text, email, company, tel, tijdstip } = data;
 
   const msg = {
-    to: 'contact@menefex.nl', // Change to your recipient
-    from: process.env.SENDGRID_AUTHORIZED_EMAIL, // Change to your verified sender
-    subject: `Mail ontvangen betreft: '${subject || tijdstip}'`,
-    text,
-    html: `<p><strong>Lead ontvangen</strong> <small>van</small> <strong>${name}</strong> - ${
-      email || '*geen email'
-    } - ${company || '*geen bedrijf'}</p>
+    from: `Menefex WMB &lt;${process.env.SENDGRID_AUTHORIZED_EMAIL}&gt;`, // Change to your verified sender
+    // subject: `Mail ontvangen betreft: '${subject || tijdstip}'`,
+    // text,
+    // html: `<p><strong>Lead ontvangen</strong> <small>van</small> <strong>${name}</strong> - ${
+    //   email || '*geen email'
+    // } - ${company || '*geen bedrijf'}</p>
 
-      <p><u><strong><small>Bericht</small></strong></u><br />${text}</p>
-   
-      <p><small>Wilt graag teruggebeld worden</small> <i><strong>${
-        tijdstip || '*geen tijdstip'
-      }
-      </strong></i></p>
-      
-      <p><small>Telnr:</small> <strong>${tel}</strong></p>`,
+    //   <p><u><strong><small>Bericht</small></strong></u><br />${text}</p>
+
+    //   <p><small>Wilt graag teruggebeld worden</small> <i><strong>${
+    //     tijdstip || '*geen tijdstip'
+    //   }
+    //   </strong></i></p>
+
+    //   <p><small>Telnr:</small> <strong>${tel}</strong></p>`,
+    templateId: 'd-8eebc10097fd430787de9cd0f3db702b',
+    personalizations: [
+      {
+        to: 'contact@menefex.nl', // Change to your recipient
+        dynamicTemplateData: {
+          name,
+          subject,
+          text,
+          email,
+          company,
+          tel,
+          tijdstip,
+        },
+      },
+    ],
   };
 
   try {
