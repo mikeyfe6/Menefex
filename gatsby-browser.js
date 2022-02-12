c8e2c83fa3a567ca0a85047741fa21a7f348e84f
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2022-02-12 16:52:09 +0100

    updated packages and ADS project image

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,3 @@ require('bootstrap/dist/css/bootstrap.min.css');
 exports.onServiceWorkerUpdateReady = () => {
   window.location.reload();
 };
-
-// export function onClientEntry() {
-//   document.querySelector('body').style.display = 'none';
-//   // console.log('GXWMB: Aan het laden ...');
-// }
-
-// export function onInitialClientRender() {
-//   document.querySelector('body').style.display = 'block';
-//   // console.log('GXWMB: Welkom bij Gimmix Webmediabedrijf !');
-// }
