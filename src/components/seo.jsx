8841cbded7bd8f296ecd57c2d14d944f22832b05
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-09-14 16:39:55 +0200

    working on improv+ some lighthouse reports

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -73,12 +73,34 @@ const SEO = ({
 
       {/* --- Link Tags ! --- */}
 
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
       <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="anonymous"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
         rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
+        rel="stylesheet"
+      />
+
+      <link
+        href="https://fonts.googleapis.com/css2?family=Righteous&display=swap"
+        rel="stylesheet"
+      />
+
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css"
+        strategy="lazyOnload"
         integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
+        crossOrigin="anonymous"
+        referrerPolicy="no-referrer"
       />
 
       {/* --- SchemaMarkup ! --- */}
