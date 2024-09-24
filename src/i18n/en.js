5e9a5e11cee3663821c3ea0b3d3bcf0b84e4abb5
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-09-24 19:25:56 +0200

    some visual changes

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -3,7 +3,7 @@ export default {
     '<strong>Sorry!</strong> The screen of your device is too small to display the website correctly...',
 
   cookieText:
-    "This site places cookies on your computer to optimize your website visit and offer personalized services, both here and through other media. Consult our <a href='/privacy-policy/' style='color: #FFCC00;'> Privacy Policy </a> for more information about the cookies used. <small> We do not track your data <b>not</b> during site visits, but to meet your preferences, we use some cookies so that you do not have to make this choice repeatedly.</small>",
+    "This site places cookies on your computer to optimize your website visit and offer personalized services, both here and through other media. Consult our <a href='/privacy-policy/' style='color: #FFCC00;'> Privacy Policy </a> for more information about the cookies used. <span> We do not track your data <b>not</b> during site visits, but to meet your preferences, we use some cookies so that you do not have to make this choice repeatedly.</span>",
 
   cookieAccept: 'Accept',
   cookieDecline: 'Decline',
--- src/i18n/nl.js
+++ src/i18n/nl.js
@@ -3,7 +3,7 @@ export default {
     '<strong>Sorry!</strong> De scherm van je toestel is te klein om de website juist weer te geven...',
 
   cookieText:
-    "Deze site plaatst cookies op je computer om je websitebezoek te optimaliseren en gepersonaliseerde diensten aan te bieden, zowel hier als via andere media. Raadpleeg ons <a href='/privacy-policy/' style='color: #FFCC00;'> Privacybeleid </a> voor meer informatie over de gebruikte cookies. <small> We volgen je gegevens <b>niet</b> bij sitebezoeken, maar om aan je voorkeuren te voldoen, gebruiken we enkele cookies zodat je deze keuze niet herhaaldelijk hoeft te maken.</small>",
+    "Deze site plaatst cookies op je computer om je websitebezoek te optimaliseren en gepersonaliseerde diensten aan te bieden, zowel hier als via andere media. Raadpleeg ons <a href='/privacy-policy/' style='color: #FFCC00;'> Privacybeleid </a> voor meer informatie over de gebruikte cookies. <span> We volgen je gegevens <b>niet</b> bij sitebezoeken, maar om aan je voorkeuren te voldoen, gebruiken we enkele cookies zodat je deze keuze niet herhaaldelijk hoeft te maken.</span>",
 
   cookieAccept: 'Accepteer',
   cookieDecline: 'Weiger',
