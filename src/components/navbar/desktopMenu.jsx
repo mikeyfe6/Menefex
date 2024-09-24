ac12b875a2967d6d030f2cf070c49a4145486e08
Michael Fransman <30863830+mikeyfe6@users.noreply.github.com>
2024-09-24 20:00:46 +0200

    jeetjee man.....

--- src/components/navbar/desktopMenu.jsx
+++ src/components/navbar/desktopMenu.jsx
@@ -127,7 +127,7 @@ const DesktopMenu = ({ drawerClickHandler }) => {
             >
               nl
             </button>
-            <span>-</span>
+            <span>|</span>
             <button
               onClick={() => switchLanguage('en')}
               className={
